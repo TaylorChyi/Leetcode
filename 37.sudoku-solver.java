258a4e99b68483013814a1c19378c57401c4fca4
Qi Tianyue <taylor_chyi@icloud.com>
2023-11-28 17:31:30 +0800

    - 37 正确版本，但是待优化

--- 37.sudoku-solver.java
+++ 37.sudoku-solver.java
@@ -33,48 +33,51 @@ class Solution {
                 }
             }
         }
+
+        // for (int i = 0; i < 9; i++) {
+        //     System.out.println(rows[i].size());
+        //     System.out.println(columns[i].size());
+        //     System.out.println(grids[i].size());
+        // }
+        
         
         backTrackSolveSudoku(0, 0, board, rows, columns, grids);
     }
 
     public boolean backTrackSolveSudoku(int i, int j, char[][] board, Set<Character>[] rows, Set<Character>[] columns, Set<Character>[] grids) {
-
-        for (; i < 9; i++) {
-            for (; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    for (char k = '1'; k <= '9'; k++) {
-                        if (isValid(i, j, k, rows, columns, grids)) {
-                            board[i][j] = k;
-                            if (backTrackSolveSudoku(i, j, board, rows, columns, grids)) 
-                                return true;
-                            else
-                                board[i][j] = '.';
-                        }            
-                    }
-                    return false;
-                }
-            }
-            j = 0;
-        }
-
-        return true;
-    }
-    
-    public boolean isValid(int row, int col, char c, Set<Character>[] rows, Set<Character>[] columns, Set<Character>[] grids) {
-
-        if (rows[row].contains(c) && 
-            columns[col].contains(c) && 
-            grids[(row / 3) * 3 + col / 3].contains(c)) {
-
-            rows[row].remove(c);
-            columns[col].remove(c);
-            grids[row / 3 * 3 + col / 3].remove(c);
+        if (i == 9) { // If the end of the board is reached, the sudoku is solved
             return true;
         }
-        else {
-            return false;
+
+        if (board[i][j] != '.') { // If the cell is not empty, continue with the next cell
+            return backTrackSolveSudoku(j == 8 ? (i + 1) : i, (j + 1) % 9, board, rows, columns, grids);
         }
+
+        for (char k = '1'; k <= '9'; k++) { // Try placing '1' to '9' in the current cell
+            if (rows[i].contains(k) &&
+            columns[j].contains(k) &&
+            grids[(i / 3) * 3 + j / 3].contains(k)) {
+                board[i][j] = k;
+                rows[i].remove(k); // Remove the character from the available set
+                columns[j].remove(k); // Remove the character from the available set
+                grids[(i / 3) * 3 + j / 3].remove(k); // Remove the character from the available set
+
+                if (backTrackSolveSudoku(j == 8 ? (i + 1) : i, (j + 1) % 9, board, rows, columns, grids)) {
+                    return true;
+                }
+
+                // Backtrack if placing k doesn't lead to a solution
+                board[i][j] = '.';
+                rows[i].add(k); // Add the character back to the available set
+                columns[j].add(k); // Add the character back to the available set
+                grids[(i / 3) * 3 + j / 3].add(k); // Add the character back to the available set
+            }
+        }
+
+        return false; // Return false if no valid number can be placed in the current cell
     }
+
+
 }
 // @lc code=end
 

72def9d498893d960971d8bc041b3caed3b583ff
Qi Tianyue <taylor_chyi@icloud.com>
2023-11-28 19:07:42 +0800

    - 52 - 完成了 n皇后问题，在gpt的帮助下； - 在解法上采用了更精简的解法，减少了内存的占用并加快了运行速度；

--- 37.sudoku-solver.java
+++ 37.sudoku-solver.java
@@ -3,81 +3,91 @@
  *
  * [37] Sudoku Solver
  */
-
 // @lc code=start
 import java.util.*;
 class Solution {
+    // Class to represent a cell with its row, column and the possible candidates.
+    private class Cell {
+        int row, col, candidates;
+
+        Cell(int row, int col, int candidates) {
+            this.row = row;
+            this.col = col;
+            this.candidates = candidates;
+        }
+    }
+
     public void solveSudoku(char[][] board) {
-        Set<Character>[] rows = new HashSet[9];
-        Set<Character>[] columns = new HashSet[9];
-        Set<Character>[] grids = new HashSet[9];
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] columns = new boolean[9][9];
+        boolean[][] grids = new boolean[9][9];
+        List<Cell> emptyCells = new ArrayList<>();
 
-        Set<Character> standerdSet = new HashSet<>();
-
-        for (char item = '1'; item <= '9'; item++) {
-            standerdSet.add(item);
-        }
-
-        for (int i = 0; i < 9; i++) {
-            rows[i] = new HashSet<>(standerdSet);
-            columns[i] = new HashSet<>(standerdSet);
-            grids[i] = new HashSet<>(standerdSet);
-        }
-
+        // Initialize with the existing numbers.
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    rows[i].remove(board[i][j]);
-                    columns[j].remove(board[i][j]);
-                    grids[i / 3 * 3 + j / 3].remove(board[i][j]);
+                if (board[i][j] == '.') {
+                    emptyCells.add(new Cell(i, j, 0));
+                } else {
+                    int num = board[i][j] - '1';
+                    rows[i][num] = true;
+                    columns[j][num] = true;
+                    grids[(i / 3) * 3 + j / 3][num] = true;
                 }
             }
         }
 
-        // for (int i = 0; i < 9; i++) {
-        //     System.out.println(rows[i].size());
-        //     System.out.println(columns[i].size());
-        //     System.out.println(grids[i].size());
-        // }
-        
-        
-        backTrackSolveSudoku(0, 0, board, rows, columns, grids);
+        // Calculate candidates for each empty cell.
+        for (Cell cell : emptyCells) {
+            cell.candidates = getCandidates(cell.row, cell.col, rows, columns, grids);
+        }
+
+        // Sort by the number of candidates to apply MRV heuristic.
+        Collections.sort(emptyCells, Comparator.comparingInt(a -> Integer.bitCount(a.candidates)));
+
+        backTrackSolveSudoku(board, rows, columns, grids, emptyCells, 0);
     }
 
-    public boolean backTrackSolveSudoku(int i, int j, char[][] board, Set<Character>[] rows, Set<Character>[] columns, Set<Character>[] grids) {
-        if (i == 9) { // If the end of the board is reached, the sudoku is solved
+    private boolean backTrackSolveSudoku(char[][] board, boolean[][] rows, boolean[][] columns, boolean[][] grids, List<Cell> emptyCells, int index) {
+        if (index == emptyCells.size()) {
             return true;
         }
 
-        if (board[i][j] != '.') { // If the cell is not empty, continue with the next cell
-            return backTrackSolveSudoku(j == 8 ? (i + 1) : i, (j + 1) % 9, board, rows, columns, grids);
-        }
+        Cell cell = emptyCells.get(index);
+        int row = cell.row, col = cell.col;
+        for (int num = 1; num <= 9; num++) {
+            if (isSafe(num, row, col, rows, columns, grids)) {
+                board[row][col] = (char) (num + '0');
+                rows[row][num - 1] = true;
+                columns[col][num - 1] = true;
+                grids[(row / 3) * 3 + col / 3][num - 1] = true;
 
-        for (char k = '1'; k <= '9'; k++) { // Try placing '1' to '9' in the current cell
-            if (rows[i].contains(k) &&
-            columns[j].contains(k) &&
-            grids[(i / 3) * 3 + j / 3].contains(k)) {
-                board[i][j] = k;
-                rows[i].remove(k); // Remove the character from the available set
-                columns[j].remove(k); // Remove the character from the available set
-                grids[(i / 3) * 3 + j / 3].remove(k); // Remove the character from the available set
-
-                if (backTrackSolveSudoku(j == 8 ? (i + 1) : i, (j + 1) % 9, board, rows, columns, grids)) {
+                if (backTrackSolveSudoku(board, rows, columns, grids, emptyCells, index + 1)) {
                     return true;
                 }
 
-                // Backtrack if placing k doesn't lead to a solution
-                board[i][j] = '.';
-                rows[i].add(k); // Add the character back to the available set
-                columns[j].add(k); // Add the character back to the available set
-                grids[(i / 3) * 3 + j / 3].add(k); // Add the character back to the available set
+                // Undo the move.
+                rows[row][num - 1] = false;
+                columns[col][num - 1] = false;
+                grids[(row / 3) * 3 + col / 3][num - 1] = false;
+                board[row][col] = '.';
             }
         }
-
-        return false; // Return false if no valid number can be placed in the current cell
+        return false;
     }
 
+    private boolean isSafe(int num, int row, int col, boolean[][] rows, boolean[][] columns, boolean[][] grids) {
+        return !rows[row][num - 1] && !columns[col][num - 1] && !grids[(row / 3) * 3 + col / 3][num - 1];
+    }
 
+    private int getCandidates(int row, int col, boolean[][] rows, boolean[][] columns, boolean[][] grids) {
+        int candidates = 0;
+        for (int num = 1; num <= 9; num++) {
+            if (!rows[row][num - 1] && !columns[col][num - 1] && !grids[(row / 3) * 3 + col / 3][num - 1]) {
+                candidates |= (1 << (num - 1));
+            }
+        }
+        return candidates;
+    }
 }
 // @lc code=end
-

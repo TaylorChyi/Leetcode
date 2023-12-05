6e2db8dac73235565caead1c862a9cfac58e9a32
Qi Tianyue <taylor_chyi@icloud.com>
2023-12-05 14:10:20 +0800

    - 缩短对角线占用的空间

--- 51.n-queens.java
+++ 51.n-queens.java
@@ -12,8 +12,8 @@ class Solution {
         List<List<String>> result = new ArrayList<>();
         int[] board = new int[n];
         boolean[] cols = new boolean[n]; // 列是否被占用
-        boolean[] diag1 = new boolean[2 * n]; // 正对角线
-        boolean[] diag2 = new boolean[2 * n]; // 反对角线
+        boolean[] diag1 = new boolean[2 * n - 1]; // 正对角线
+        boolean[] diag2 = new boolean[2 * n - 1]; // 反对角线
 
         backTrackSolveNQueens(0, board, result, cols, diag1, diag2);
         return result;
@@ -25,9 +25,10 @@ class Solution {
             return;
         }
 
+        int d1, d2;
         for (int col = 0; col < board.length; col++) {
-            int d1 = row - col + board.length;
-            int d2 = row + col;
+            d1 = row - col + board.length - 1;
+            d2 = row + col;
             if (cols[col] || diag1[d1] || diag2[d2]) continue;
 
             board[row] = col;
@@ -42,8 +43,9 @@ class Solution {
 
     private List<String> construct(int[] board) {
         List<String> res = new ArrayList<>();
+        char[] temp;
         for (int i = 0; i < board.length; i++) {
-            char[] temp = new char[board.length];
+            temp = new char[board.length];
             for (int j = 0; j < board.length; j++) {
                 if (board[i] == j) {
                     temp[j] = 'Q';
